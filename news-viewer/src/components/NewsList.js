c92d2a9e62a48d6309fda8c0b205ee32992a246c
DongwookJeong <nolhoon211@gmail.com>
2023-05-01 22:57:37 +0900

    카테고리 별 api 호출

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -17,7 +17,7 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const NewsList = () => {
+const NewsList = ({category}) => {
     const [articles, setArticles] = useState(null)
     const [loading, setLoading] = useState(false)
 
@@ -26,8 +26,9 @@ const NewsList = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`
                 const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`,
+                    `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=${key}`,
                 )
                 setArticles(response.data.articles)
             } catch (e) {
@@ -36,7 +37,7 @@ const NewsList = () => {
             setLoading(false)
         }
         fetchData();
-    }, [])
+    }, [category])
 
     // ! 대기 중일 때
     if (loading){
