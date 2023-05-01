9266753a544c1d390ccdb89fe42f3d997401f0f1
DongwookJeong <nolhoon211@gmail.com>
2023-05-01 22:26:35 +0900

    카테고리 추가

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -27,7 +27,7 @@ const NewsList = () => {
             setLoading(true);
             try {
                 const response = await axios.get(
-                    `http://newsapi.org/v2/top-headlines?country=kr&apiKey=${key}`,
+                    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`,
                 )
                 setArticles(response.data.articles)
             } catch (e) {
